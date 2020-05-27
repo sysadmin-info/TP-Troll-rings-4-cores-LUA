1585c3598b7aa0834b5f2e31ce930800bf0d518b
Adrian <adrian.ambroziak@gmail.com>
2020-05-27 22:15:10 +0200

    Modified TP-Troll to modern conky settings requirements

--- conky-delay.sh
+++ conky-delay.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-conky -c /home/adrian/.conky/TP-Troll-rings-4-cores-LUA.conkyrc & sleep 1s
-conky -c /home/adrian/.conky/TP-Troll-Cpu.conkyrc & sleep 1s
-conky -c /home/adrian/.conky/TP-Troll-Mem.conkyrc & sleep 1s
+conky -c ~/.conky/TP-Troll-rings-4-cores-LUA.conkyrc & sleep 1s
+conky -c ~/.conky/TP-Troll-Cpu.conkyrc & sleep 1s
+conky -c ~/.conky/TP-Troll-Mem.conkyrc & sleep 1s
 exit
